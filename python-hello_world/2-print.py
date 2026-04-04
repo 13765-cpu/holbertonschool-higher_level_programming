0346c8b18593b98932894dd61a4ef7a6ad6859ed
Rahima <13765@holbertonstudent.com>
2026-04-04 19:28:30 +0000

    add 2-print.py script

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Prints "Programming is like building a multilingual puzzle" followed by a new line
 
-print("Programming is like building a multilingual puzzle")
+print('"Programming is like building a multilingual puzzle')
