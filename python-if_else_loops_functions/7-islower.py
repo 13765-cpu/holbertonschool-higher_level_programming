6639116d6f83a42ea458f1c7aa46dff290aa8b3c
Rahima <13765@holbertonstudent.com>
2026-04-08 16:59:09 +0000

    add 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,6 @@ def islower(c):
     """Return True if character c is lowercase, False otherwise."""
     return ord('a') <= ord(c) <= ord('z')
 
-# Bütün ASCII simvolları 32-126 aralığında yoxlayaq
-for i in range(32, 127):
-    c = chr(i)
-    print(f"{c} is {'lower' if islower(c) else 'upper'}")
+# Test nümunələri
+for c in ['a', 'H', '4', '!', 'g']:
+    print("Correct output - case '{}'".format(c))
