5361b2e60f885ff33ff593bb10a303c8cb00f27a
Rahima <13765@holbertonstudent.com>
2026-04-09 10:40:06 +0000

    add 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """Siyahıdan müəyyən indeksdəki elementi qaytarır."""
     
-    # İndeks mənfi olarsa None qaytar
     if idx < 0:
         return None
-    
-    # İndeks siyahının uzunluğundan böyük və ya bərabərdirsə None qaytar
-    # (Siyahı indeksləri 0-dan başladığı üçün len(my_list) - 1 sonuncu indeksdir)
+
     if idx >= len(my_list):
         return None
     
-    # Əgər yuxarıdakı şərtlər ödənmirsə, elementi qaytar
     return my_list[idx]
