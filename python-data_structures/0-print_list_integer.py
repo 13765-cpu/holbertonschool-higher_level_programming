5e795251e0dae1472eef14e2cf8aa545ed5961b2
Rahima <13765@holbertonstudent.com>
2026-04-09 10:34:34 +0000

    add 0-print_list_integer.py

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Siyahıdakı tam ədədləri hər sətirdə bir dənə olmaqla çap edir."""
+    for i in my_list:
+        print("{:d}".format(i))
