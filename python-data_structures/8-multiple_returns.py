4c49304e776cdd5dd4f4cc24dd9d3d95313fc676
Rahima <13765@holbertonstudent.com>
2026-04-10 13:46:30 +0000

    add 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # Cümlənin uzunluğunu hesablayırıq
     length = len(sentence)
-    
-    # Əgər cümlə boşdursa (uzunluq 0-dırsa), ilk xarakter None olur
     if length == 0:
         first_char = None
     else:
-        # Əks halda ilk xarakteri götürürük
         first_char = sentence[0]
-    
-    # Nəticəni tuple şəklində qaytarırıq
     return (length, first_char)
