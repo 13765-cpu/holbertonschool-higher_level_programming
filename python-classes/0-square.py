2b2a8ee16b69e0c64ad06feb1ead8aea83e269f8
Rahima <13765@holbertonstudent.com>
2026-04-11 14:36:41 +0000

    add 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+"""Kvadratı təyin edən modul.
 class Square:
+    """Kvadratı təmsil edən boş klass
     pass
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
