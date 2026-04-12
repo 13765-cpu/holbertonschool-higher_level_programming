11f4675e48bbb86d55c9886118e81e15c0e36210
Rahima <13765@holbertonstudent.com>
2026-04-12 06:59:06 +0000

    add 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Square klassını təyin edən modul."""
+
+
 class Square:
+    """Kvadratı təmsil edən klass."""
+
     def __init__(self, size):
+        """Kvadratı müəyyən bir ölçü ilə yaradır.
+
+        Args:
+            size (int): Kvadratın tərəfinin 
+        """
         self.__size = size
