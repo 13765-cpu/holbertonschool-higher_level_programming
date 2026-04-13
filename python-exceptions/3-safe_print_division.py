01cc6b9d836a6bf5072a35638a0dd681ab2d611a
Rahima <13765@holbertonstudent.com>
2026-04-13 18:57:55 +0000

    add 3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
     result = None
     try:
         result = a / b
     except ZeroDivisionError:
-        # Sıfıra bölmə halında result None olaraq qalır
         pass
     finally:
-        # Nə baş verməsindən asılı olmayaraq nəticə burada çap olunur
         print("Inside result: {}".format(result))
-    
     return result
