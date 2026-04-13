5e6d85c6ac9b458f10af5b6ea61b21cebbb7a8b9
Rahima <13765@holbertonstudent.com>
2026-04-13 19:13:15 +0000

    add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,17 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            # 1. İlk öncə elementin mövcudluğunu və tipini eyni anda yoxlayırıq
-            value = my_list[i]
-            try:
-                print("{:d}".format(value), end="")
-                count += 1
-            except (ValueError, TypeError):
-                # Integer deyilsə səssizcə keçirik
-                continue
-        except IndexError:
-            # Siyahı bitəndə IndexError buraya düşür və dövrü dayandırırıq
-            break
-            
-    print("") # Yeni sətir
+            # Siyahıdan kənara çıxanda (IndexError) bura heç bir except-ə girmir
+            # Və birbaşa proqramın dayanmasına (Traceback) səbəb olur
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            # Yalnız tip xətalarını tuturuq (string-ləri keçmək üçün)
+            pass
+    print("")
     return count
