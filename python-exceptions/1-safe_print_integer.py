1bf962f6c9ffebae29ac7e6338afbbd3999f5a5d
Rahima <13765@holbertonstudent.com>
2026-04-13 17:11:49 +0000

    add 1-safe_print_integer.py

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        # {:d} yalnız integer çap edə bilir
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        # Əgər value integer deyilsə, bura keçid edir
+        return False
