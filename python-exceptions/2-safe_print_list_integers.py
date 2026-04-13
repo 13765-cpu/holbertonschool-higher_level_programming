e7be6328784c5567f1c10fa4049660bdc776e702
Rahima <13765@holbertonstudent.com>
2026-04-13 18:45:30 +0000

    add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,17 +4,14 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            # Əvvəlcə integer olub-olmadığını format ilə yoxlayırıq
+            # {:d} vasitəsilə yoxlayırıq: əgər integer-dirsə çap edirik
             print("{:d}".format(my_list[i]), end="")
-            # Əgər yuxarıdakı sətir xəta vermədisə, deməli çap etdik
             count += 1
         except (ValueError, TypeError):
-            # Integer deyilsə, sadəcə növbəti elementə keçirik
+            # Əgər element integer deyilsə, səssizcə keçirik (skip)
             continue
         except IndexError:
-            # Əgər siyahı bitdisə, dövrü tamamilə dayandırırıq
+            # Əgər x siyahının uzunluğundan böyükdürsə, dövrü dayandırırıq
             break
-    
-    # Dövr bitəndə (xəta olsa da, olmasa da) mütləq yeni sətir
-    print("")
+    print("")  # Sonda yeni sətir
     return count
