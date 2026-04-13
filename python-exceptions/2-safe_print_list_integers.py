a548cc05ec787fe3ffb86005cc9d84dd1d1803f3
Rahima <13765@holbertonstudent.com>
2026-04-13 19:14:38 +0000

    add 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            # Siyahıdan kənara çıxanda (IndexError) bura heç bir except-ə girmir
-            # Və birbaşa proqramın dayanmasına (Traceback) səbəb olur
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            # Yalnız tip xətalarını tuturuq (string-ləri keçmək üçün)
             pass
     print("")
     return count
