05cc96f15368c02fbccf75f0de8fe81496962022
Rahima <13765@holbertonstudent.com>
2026-04-17 11:38:59 +0000

    add  1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 import sys
 import urllib.request
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
-        print(response.headers.get("X-Request-Id"))
+req = urllib.request.Request(url, headers={"cfclearance": "true"})
+
+with urllib.request.urlopen(req) as response:
+    print(response.headers.get("X-Request-Id"))
