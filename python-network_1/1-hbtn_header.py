d13d831e974699cbc980b41bce61debdf277ecea
Rahima <13765@holbertonstudent.com>
2026-04-17 11:48:05 +0000

    add 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        print(response.headers.get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        request_id = headers.get("X-Request-Id")
+        print(request_id)
