2c5995a5f0b01ae7cb2cd74acc67b65d5ccadbf2
Rahima <13765@holbertonstudent.com>
2026-04-17 10:43:59 +0000

    add 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display body only if HTTP status code is 200
-[ "$(curl -s -o /dev/null -w "%{http_code}" "http://$1")" = "200" ] && curl -s "http://$1"
+# Display body of response
+curl -s "http://$1"
