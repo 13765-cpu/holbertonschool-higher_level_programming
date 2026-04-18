7dabc38f8c307a1994aae20e879a191b6f9bc707
Rahima <13765@holbertonstudent.com>
2026-04-18 10:16:27 +0000

    add 6-post_email.py

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that sends a POST request with an email and displays the response body"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Command line-dan URL və email götürürük
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # POST üçün data dictionary şəklində göndərilir
+    data = {'email': email}
+
+    # POST request göndəririk
+    response = requests.post(url, data=data)
+
+    # Response body-ni çap edirik
+    print(response.text)
